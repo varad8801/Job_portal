a4fefbb520eda561683430908583bc5ca510092b
varad <sardeshpandevarad2001@gmail.com>
2023-12-11 11:48:09 +0530

    update functionality added

--- app.py
+++ app.py
@@ -69,6 +69,42 @@ def job_details(job_id):
         job = cursor.fetchone()
     return render_template('job_details.html', job=job)
 
+
+@app.route('/update_job/<int:job_id>', methods=['GET', 'POST'])
+def update_job(job_id):
+
+
+    with mysql.connector.connect(**DB_CONFIG) as connection:
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM jobs WHERE id = %s', (job_id,))
+        job = cursor.fetchone()
+
+    if not job:
+        return 'Job not found', 404
+
+    if request.method == 'POST':
+        # Extract updated job details from the form
+        updated_title = request.form.get('title')
+        updated_category = request.form.get('category')
+        updated_description = request.form.get('description')
+        updated_employer = request.form.get('employer')
+        updated_email = request.form.get('email')
+        # Update the job in the database
+        with mysql.connector.connect(**DB_CONFIG) as connection:
+            cursor = connection.cursor()
+            update_query = """
+                UPDATE jobs
+                SET title = %s, category = %s ,description=%s, employer = %s, email = %s
+                WHERE id = %s
+            """
+            cursor.execute(update_query, (updated_title, updated_category, updated_description, updated_employer, updated_email, job_id))
+            connection.commit()
+
+        return redirect(url_for('all_jobs'))
+
+    return render_template('update_job.html', job=job)
+
+
 @app.route('/all_jobs')
 def all_jobs():
     if 'username' not in session:
